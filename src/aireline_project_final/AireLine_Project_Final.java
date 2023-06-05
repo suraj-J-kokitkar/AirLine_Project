e9b23ed24ca5d29a0a97ef704dcb79123607b068
suraj-J-kokitkar <kokitkar.suraj5@gmail.com>
2023-06-05 20:37:53 +0530

    AirLine

---
+++ src/aireline_project_final/AireLine_Project_Final.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aireline_project_final;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class AireLine_Project_Final {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
